c18437e6173585c659fc2167dac53ab34f913ad8
jonathan <148167.jw@gmail.com>
2022-12-06 08:02:23 +0100

    Day6 File Cleanup

--- solutions/day6.py
+++ solutions/day6.py
@@ -3,15 +3,8 @@ from readFile import *
 
 def solution(datastream, n_distinct_characters):
     for i in range(n_distinct_characters + 1, len(datastream)):
-       if len(set(datastream[i-n_distinct_characters:i])) == n_distinct_characters:
-           return i
-    return 'Nothing found'
-
-
-def part2(datastream):
-    for i in range(3, len(datastream)):
-        if len(set(datastream[i - 3:i + 1])) == 4:
-            return i + 1
+        if len(set(datastream[i-n_distinct_characters:i])) == n_distinct_characters:
+            return i
     return 'Nothing found'
 
 

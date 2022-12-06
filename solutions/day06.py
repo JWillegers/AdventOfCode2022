b02fa68e247565ef7b54dfee1f89b5e31a0ad29b
jonathan <148167.jw@gmail.com>
2022-12-06 10:30:14 +0100

    Day6 one-line

--- solutions/day06.py
+++ solutions/day06.py
@@ -8,7 +8,13 @@ def solution(datastream, n_distinct_characters):
     return 'Nothing found'
 
 
+def one_line(datastream, n_distinct_characters):
+    return min([(i if len(set(datastream[i-n_distinct_characters:i])) == n_distinct_characters else 999999) for i in range(n_distinct_characters + 1, len(datastream))])
+
+
 if __name__ == '__main__':
     file = line_str(6)
+    assert(solution(file[0], 4) == one_line(file[0], 4))
+    assert(solution(file[0], 14) == one_line(file[0], 14))
     print('part1:', solution(file[0], 4))
     print('part2:', solution(file[0], 14))

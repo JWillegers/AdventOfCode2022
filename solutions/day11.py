d564e3ee994fd73d379a523ed6cb2acef6f6c566
jonathan <148167.jw@gmail.com>
2022-12-11 17:55:33 +0100

    Day11 Finished

--- solutions/day11.py
+++ solutions/day11.py
@@ -1,33 +1,34 @@
+import copy
 import math
 import re
 from readFile import *
 
 
-def part1(monkey_items, monkey_data, needed_rounds, factor_3):
-    inspections = [0 for monkey in range(8)]
+def solution(n_of_monkeys, monkey_items, monkey_data, needed_rounds, factor_3, lcm):
+    inspections = [0 for monkey in range(n_of_monkeys)]
     for throw_round in range(needed_rounds):
         for index in range(len(monkey_data)):
             monkey = monkey_data[index]
             # Structure of if-statements only works because 'Operation: new = old + old' doesn't exists
             if monkey[1]:
-                monkey_items, inspections = monkey_factor(monkey_items, inspections, index, monkey[3], monkey[4], monkey[5], factor_3)
+                monkey_items, inspections = monkey_factor(monkey_items, inspections, index, monkey[3], monkey[4], monkey[5], factor_3, lcm)
             elif monkey[0]:
-                monkey_items, inspections = monkey_multiplication(monkey_items, inspections, index, monkey[2], monkey[3], monkey[4], monkey[5], factor_3)
+                monkey_items, inspections = monkey_multiplication(monkey_items, inspections, index, monkey[2], monkey[3], monkey[4], monkey[5], factor_3, lcm)
             else:
-                monkey_items, inspections = monkey_addition(monkey_items, inspections, index, monkey[2], monkey[3], monkey[4], monkey[5], factor_3)
+                monkey_items, inspections = monkey_addition(monkey_items, inspections, index, monkey[2], monkey[3], monkey[4], monkey[5], factor_3, lcm)
     highest_value = max(inspections)
     inspections.remove(highest_value)
     return highest_value * max(inspections)
 
 
-def monkey_factor(monkey_items, inspections, monkey_index, test, monkey_true, monkey_false, factor_3):
+def monkey_factor(monkey_items, inspections, monkey_index, test, monkey_true, monkey_false, factor_3, lcm):
     while len(monkey_items[monkey_index]) > 0:
         inspections[monkey_index] += 1
         old = monkey_items[monkey_index].pop(0)
         if factor_3:
             item = math.floor((old * old) / 3)
         else:
-            item = old * old
+            item = (old * old) % lcm
         if item % test == 0:
             monkey_items[monkey_true].append(item)
         else:
@@ -35,13 +36,13 @@ def monkey_factor(monkey_items, inspections, monkey_index, test, monkey_true, mo
     return monkey_items, inspections
 
 
-def monkey_multiplication(monkey_items, inspections, monkey_index, factor, test, monkey_true, monkey_false, factor_3):
+def monkey_multiplication(monkey_items, inspections, monkey_index, factor, test, monkey_true, monkey_false, factor_3, lcm):
     while len(monkey_items[monkey_index]) > 0:
         inspections[monkey_index] += 1
         if factor_3:
             item = math.floor((monkey_items[monkey_index].pop(0) * factor) / 3)
         else:
-            item = monkey_items[monkey_index].pop(0) * factor
+            item = (monkey_items[monkey_index].pop(0) * factor) % lcm
         if item % test == 0:
             monkey_items[monkey_true].append(item)
         else:
@@ -49,13 +50,13 @@ def monkey_multiplication(monkey_items, inspections, monkey_index, factor, test,
     return monkey_items, inspections
 
 
-def monkey_addition(monkey_items, inspections, monkey_index, add, test, monkey_true, monkey_false, factor_3):
+def monkey_addition(monkey_items, inspections, monkey_index, add, test, monkey_true, monkey_false, factor_3, lcm):
     while len(monkey_items[monkey_index]) > 0:
         inspections[monkey_index] += 1
         if factor_3:
             item = math.floor((monkey_items[monkey_index].pop(0) + add) / 3)
         else:
-            item = monkey_items[monkey_index].pop(0) + add
+            item = (monkey_items[monkey_index].pop(0) + add) % lcm
         if item % test == 0:
             monkey_items[monkey_true].append(item)
         else:
@@ -63,9 +64,8 @@ def monkey_addition(monkey_items, inspections, monkey_index, add, test, monkey_t
     return monkey_items, inspections
 
 
-if __name__ == '__main__':
-    file = line_str(11)
-    global_monkey_items = []
+def parse_file(filez, n_monkeys):
+    monkey_items = []
     ''''
     global_monkey_data structure:
     [
@@ -77,31 +77,44 @@ if __name__ == '__main__':
     throws false: int
     ]
     '''
-    global_monkey_data = [[] for monkey in range(4)]
-    for i in range(1, len(file)):
+    monkey_data = [[] for monkey in range(n_monkeys)]
+    for i in range(1, len(filez)):
         global_index = i // 7
-        line = file[i]
+        line = filez[i]
         if i % 7 == 1:
             # starting items
             numbers = line.split(':')[1][1:].split(', ')
             m = []
             for j in numbers:
                 m.append(int(j))
-            global_monkey_items.append(m)
+            monkey_items.append(m)
         elif i % 7 == 2:
             # operation
-            global_monkey_data[global_index].append('*' in line)
-            global_monkey_data[global_index].append(not bool(re.search(r'\d', line)))  # check digit in string
-            if not global_monkey_data[global_index][0] and global_monkey_data[global_index][1]:
+            monkey_data[global_index].append('*' in line)
+            monkey_data[global_index].append(not bool(re.search(r'\d', line)))  # check digit in string
+            if not monkey_data[global_index][0] and monkey_data[global_index][1]:
                 print('\'Operation: new = old + old\' is not supported')
                 exit(0)
-            if global_monkey_data[global_index][1]:
-                global_monkey_data[global_index].append(None)
+            if monkey_data[global_index][1]:
+                monkey_data[global_index].append(None)
             else:
-                global_monkey_data[global_index].append(int(line.split(' ')[-1]))
+                monkey_data[global_index].append(int(line.split(' ')[-1]))
         elif 0 < i % 7 < 6:
             # test, if true, if false
-            global_monkey_data[global_index].append(int(line.split(' ')[-1]))
+            monkey_data[global_index].append(int(line.split(' ')[-1]))
+    lcm = 1
+    for m in monkey_data:
+        lcm = math.lcm(lcm, m[3])
+    return monkey_items, monkey_data, lcm
 
-    print('part1:', part1(global_monkey_items, global_monkey_data, 20, True))
-    print('part2:', part1(global_monkey_items, global_monkey_data, 10000, False))
+
+if __name__ == '__main__':
+    test_file = line_str(11, True)
+    test_monkey_items, test_monkey_data, test_lcm = parse_file(test_file, 4)
+    assert(solution(4, copy.deepcopy(test_monkey_items), test_monkey_data, 20, True, test_lcm) == 10605)
+    print(solution(4, copy.deepcopy(test_monkey_items), test_monkey_data, 10000, False, test_lcm))
+    assert(solution(4, copy.deepcopy(test_monkey_items), test_monkey_data, 10000, False, test_lcm) == 2713310158)
+    file = line_str(11)
+    global_monkey_items, global_monkey_data, global_lcm = parse_file(file, 8)
+    print('solution:', solution(8, copy.deepcopy(global_monkey_items), global_monkey_data, 20, True, global_lcm))
+    print('part2:', solution(8, copy.deepcopy(global_monkey_items), global_monkey_data, 10000, False, global_lcm))

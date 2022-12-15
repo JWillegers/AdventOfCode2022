d8a1ef5329fd9ce21ffb227360f1fea17abb935b
jonathan <148167.jw@gmail.com>
2022-12-15 15:25:02 +0100

    Day15 completed

--- solutions/day15.py
+++ solutions/day15.py
@@ -31,8 +31,47 @@ def part1(sensors_and_beacons, interested_row):
 
 def part2(sensors_and_beacons, max_row):
     for pair in sensors_and_beacons:
-        t = True
-        # TODO walk around 'the edge'
+        x_current = pair[0][0]
+        y_current = pair[0][1] - pair[2]  # start at the top
+        first_round = True
+        dx = 1
+        dy = 1
+        while first_round or not (x_current == pair[0][0] and y_current == pair[0][1] + pair[2]):
+            # find options
+            options = []
+            if abs(x_current - 1 - pair[0][0]) + abs(y_current - pair[0][1]) > pair[2]:
+                options.append((x_current - 1, y_current))
+            if abs(x_current + 1 - pair[0][0]) + abs(y_current - pair[0][1]) > pair[2]:
+                options.append((x_current + 1, y_current))
+            if abs(x_current - pair[0][0]) + abs(y_current - 1 - pair[0][1]) > pair[2]:
+                options.append((x_current, y_current - 1))
+            if abs(x_current - pair[0][0]) + abs(y_current + 1 - pair[0][1]) > pair[2]:
+                options.append((x_current, y_current + 1))
+
+            # check options
+            for x_check, y_check in options:
+                if 0 <= x_check <= max_row and 0 <= y_check <= max_row:  # if within range
+                    found = True
+                    # compare with all other sensor ranges
+                    for other_pair in sensors_and_beacons:
+                        if other_pair is not pair:
+                            manhattan = abs(x_check - other_pair[0][0]) + abs(y_check - other_pair[0][1])
+                            if manhattan < other_pair[2]:
+                                found = False
+                    if found:
+                        return x_check * 4_000_000 + y_check
+            # change direction if needed
+            if x_current == pair[0][0] + pair[2]:
+                dx = -1
+            if y_current == pair[0][1] + pair[2]:
+                dy = -1
+            if x_current == pair[0][0] - pair[2]:
+                dx = 1
+            # move x_current and y_current to next field
+            x_current += dx
+            y_current += dy
+            first_round = False
+
     return -1
 
 
@@ -51,10 +90,9 @@ def parse(f):
 if __name__ == '__main__':
     testFile = line_str(15, True)
     test_sensors_and_beacons = parse(testFile)
-    assert(part1(test_sensors_and_beacons, 10))
-    print(part2(test_sensors_and_beacons, 20))
+    assert(part1(test_sensors_and_beacons, 10) == 26)
+    assert(part2(test_sensors_and_beacons, 20) == 56000011)
     file = line_str(15)
     global_sensors_and_beacons = parse(file)
     print('part1:', part1(global_sensors_and_beacons, 2_000_000))
-    exit(0)
     print('part2:', part2(global_sensors_and_beacons, 4_000_000))

e6915a62356de1b7e9f4a133dc24b23532bd50f5
jonathan <148167.jw@gmail.com>
2022-12-17 09:52:12 +0100

    Day16 not much progress

--- solutions/day16.py
+++ solutions/day16.py
@@ -1,3 +1,4 @@
+import math
 from readFile import *
 from copy import deepcopy
 import itertools
@@ -12,49 +13,52 @@ def part1(data):
 
     # key is as follows (start, goal)
     valves_that_we_want_open.add('AA')
-    paths = dict()
-    for x, y in itertools.combinations(valves_that_we_want_open, 2):
-        paths[(x, y)] = find_path(x, y, data)
-        paths[(y, x)] = find_path(y, x, data)
+    paths = get_paths(data, valves_that_we_want_open)
 
     # assumption that I make, if False the code will break
     assert(data['AA']['flow'] == 0)
     valves_that_we_want_open.remove('AA')
+    valves_that_we_want_open = list(valves_that_we_want_open)
 
     # (Location, time, flow per minute, total flow, opened_valves as [])
-    queue = []
-    for valve in valves_that_we_want_open:
-        queue.append((valve, len(paths[('AA', valve)]), 0, 0, []))
+    queue = [('AA', 0, 0, 0, [])]
 
     max_flow = 0
     while len(queue) > 0:
-        location, time, fpm, total_flow, opened_valves = queue.pop(0)
-        if time >= 30:
-            old_max_flow = max_flow
-            max_flow = max(max_flow, total_flow)
-            if max_flow > old_max_flow:
-                print(max_flow, opened_valves)
-        else:
-            total_flow += fpm
-            time += 1
-            fpm += data[location]['flow']
-            ov_copy = deepcopy(opened_valves)
-            ov_copy.append(location)
-            dead_end = True
-            for valve in valves_that_we_want_open:
-                if valve not in ov_copy:
-                    dead_end = False
-                    time_jump = len(paths[(location, valve)])
-                    new_total_flow = total_flow + min(time_jump, 30 - time) * fpm
-                    queue.append((valve, time + time_jump,
-                                  fpm, new_total_flow, ov_copy))
-            if dead_end:
-                old_max_flow = max_flow
-                max_flow = max(max_flow, total_flow + (30 - time) * fpm)
-                if max_flow > old_max_flow:
-                    print(max_flow, opened_valves)
-
+        location, time, fpm, total_flow, opened_valves = queue.pop()
+        max_flow, queue = process(queue, data, valves_that_we_want_open, paths, max_flow, location, time, fpm, total_flow, opened_valves)
+    print(max_flow)
     return max_flow
+
+
+def get_paths(data, valves_that_we_want_open):
+    paths = dict()
+    for x, y in itertools.combinations(valves_that_we_want_open, 2):
+        paths[(x, y)] = find_path(x, y, data)
+        paths[(y, x)] = find_path(y, x, data)
+    return paths
+
+
+def process(queue, data, valves_that_we_want_open, paths, max_flow, location, time, fpm, total_flow, opened_valves):
+    if time >= 30:
+        max_flow = max(max_flow, total_flow)
+    expected_new_flow = []
+    for valve in valves_that_we_want_open:
+        if valve is not location and valve not in opened_valves:
+            expected_new_flow.append((valve, (30 - time - len(paths[(location, valve)]) - 1) * data[valve]['flow']))
+    expected_new_flow = sorted(expected_new_flow, key=lambda x: x[1], reverse=True)
+    if len(expected_new_flow) == 0:
+        max_flow = max(max_flow, total_flow + (30 - time) * (fpm + data[location]['flow']))
+    else:
+        for i in range(min(1 + math.ceil(len(valves_that_we_want_open) / 3), len(expected_new_flow))):
+            valve = expected_new_flow[i][0]
+            new_fpm = fpm + data[location]['flow']
+            cov = deepcopy(opened_valves)
+            cov.append(valve)
+            queue.append((valve, time + len(paths[(location, valve)]) + 1, new_fpm,
+                          total_flow + min(30 - time, len(paths[(location, valve)]) + 1) * new_fpm, cov))
+    return max_flow, queue
+
 
 
 # BFS
@@ -94,6 +98,7 @@ if __name__ == '__main__':
     test_file = line_str(16, True)
     test_tunnels = parse(test_file)
     assert(part1(test_tunnels) == 1651)
+    #exit(0)
     file = line_str(16)
     global_tunnels = parse(file)
     print('part1:', part1(global_tunnels))
